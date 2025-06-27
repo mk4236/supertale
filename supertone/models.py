6389f99c93e3ede862d00ef8d72e66fc6d554682
MinQ <61265535+mk4236@users.noreply.github.com>
2025-06-27 11:30:39 +0900

    feat(supertone): 게시판 모델 SuperTone에 제목, 내용, 작성일·수정일·작성자·수정자 필드 추가

--- supertone/models.py
+++ supertone/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class SuperTone(models.Model):
+    title = models.CharField(max_length=200)
+    contents = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="supertone_created",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="supertone_updated",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
